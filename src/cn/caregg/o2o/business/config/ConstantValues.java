fbc1a20b039714d60838269f1c2070639ea9bd16
FrecelHawk <FrecelHawk@yahoo.com>
2015-07-06 15:27:43 +0800

    FrecelHawk

---
+++ src/cn/caregg/o2o/business/config/ConstantValues.java
@@ -0,0 +1,13 @@
+package cn.caregg.o2o.business.config;
+
+/**
+ * @ClassName: ConstantValues
+ * @Description: TODO  常量
+ * @author FH 
+ * @date 2015年6月30日 下午4:23:32
+ * 
+*/
+
+public interface ConstantValues {
+
+}
